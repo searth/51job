5d1a8457989f9f3bdd41dd2f39f8191f9792fa2d
searth <searth8@foxmail.com>
2021-03-02 17:22:49 +0800

    change 1

--- 51job/get_51job_data.py
+++ 51job/get_51job_data.py
@@ -1,34 +1,36 @@
 # ==================================
 # --*-- coding: utf-8 --*--
-# @Time    : 2020-07-11
+# @Time    : 2021-03-02
 # @Author  : TRHX
-# @Blog    : www.itrhx.com
-# @CSDN    : itrhx.blog.csdn.net
 # @FileName: get_51job_data.py
-# @Software: PyCharm
+# @Software: Sublime Text 3
 # ==================================
 
 
 import re
 import time
 import copy
-import pymongo
 import requests
 from lxml import etree
+import pandas as pd
 
 
 class JobSpider:
     def __init__(self):
+    	# 设置基础网页形式、浏览器请求头，输入查询关键字
         self.base_url = 'https://search.51job.com/list/000000,000000,0000,00,9,99,%s,2,%s.html'
         self.headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.13 Safari/537.36'}
         self.keyword = input('请输入关键字：')
 
     def tatal_url(self):
+    	# 补全网页
         url = self.base_url % (self.keyword, str(1))
         response = requests.get(url=url, headers=self.headers)
+        # 转换为Element对象
         tree = etree.HTML(response.content.decode('gbk'))
         # 提取一共有多少页
         text = tree.xpath("//div[@class='p_in']/span[1]/text()")[0]
+        # 等运行成功了换个方式提取数字看看(☆☆)
         number = re.findall('[0-9]', text)
         number = int(''.join(number))
         print('%s职位共有%d页' % (self.keyword, number))
@@ -39,10 +41,11 @@ class JobSpider:
         """
         1、解析每一页职位详情页的 url
         2、特殊情况一：如果有前程无忧自己公司的职位招聘信息掺杂在里面，他的详情页结构和普通的也不一样，页面编码也有差别。
-           页面示例：https://51rz.51job.com/job.html?jobid=115980776
+           页面示例：https://51rz.51job.com/job.html?jobid=115980058
            页面真实数据请求地址类似于：https://coapi.51job.com/job_detail.php?jsoncallback=&key=&sign=params={"jobid":""}
            请求地址中的各参数值通过 js 加密：https://js.51jobcdn.com/in/js/2018/coapi/coapi.min.js
-        3、特殊情况二：部分公司有自己的专属页面，此类页面的结构也不同于普通页面
+           (如果不用selenium，如何锁定这个网址，持疑，待解决☆☆☆)
+        3、特殊情况二：部分公司有自己的专属页面，此类页面的结构也不同于普通页面(和上面那个一起解决☆☆☆)
            页面示例：http://dali.51ideal.com/jobdetail.html?jobid=121746338
         4、为了规范化，本次爬取将去掉这部分特殊页面，仅爬取 url 带有 jobs.51job.com 的数据
         """
@@ -51,12 +54,14 @@ class JobSpider:
             url = self.base_url % (self.keyword, str(num))
             response = requests.get(url=url, headers=self.headers)
             tree = etree.HTML(response.content.decode('gbk'))
-            detail_url1 = tree.xpath("//div[@class='dw_table']/div[@class='el']/p/span/a/@href")
+            # xpath为"//div[@class='j_joblist']/div[@class='e']/a[@class='el']/@href"精简,有错可再修改
+            detail_url1 = tree.xpath("//a[@class='el']/@href")
 
             """
             深拷贝一个 url 列表，如果有连续的不满足要求的链接，若直接在原列表里面删除，
             则会漏掉一些链接，因为每次删除后的索引已改变，因此在原列表中提取不符合元素
             后，在深拷贝的列表里面进行删除。最后深拷贝的列表里面的元素均符合要求。
+            (我打算试试用迭代器删除列表元素☆☆)
             """
 
             detail_url2 = copy.deepcopy(detail_url1)
@@ -107,6 +112,7 @@ class JobSpider:
 
             # 经验、学历、招聘人数、发布时间等信息都在一个标签里面，逐一使用列表解析式提取
             content = tree.xpath("//div[@class='cn']/p[2]/text()")
+            # 去空格
             content = [i.strip() for i in content]
             if content:
                 region = content[0]
@@ -118,6 +124,7 @@ class JobSpider:
             publish_date = ''.join([i for i in content if '发布' in i])
             english = ''.join([i for i in content if '英语' in i])
 
+            # 词语用逗号分隔，第一个最后一个都是换行符
             welfare_tags = ','.join(tree.xpath("//div[@class='jtag']/div//text()")[1:-2])
             job_information = ''.join(tree.xpath("//div[@class='bmsg job_msg inbox']/p//text()")).replace(' ', '')
             work_address = ''.join(tree.xpath("//div[@class='bmsg inbox']/p//text()"))
@@ -131,8 +138,13 @@ class JobSpider:
                         english, welfare_tags, job_information, work_address, company_name,
                         company_nature, company_scale, company_industry, company_information]
 
-            save_mongodb(job_data)
+            deal(job_data)
 
+            #save_mongodb(job_data)
+
+def deal(q):
+	df = pd.DataFrame(a)
+	df.to_excel('data.xlsx',index = False)
 
 def save_mongodb(data):
     client = pymongo.MongoClient(host='localhost', port=27017)
